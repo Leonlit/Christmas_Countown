811f0eb45eb1735943d3fe3307bf1b62bd185ca0
Leon <leonlit123@gmail.com>
2019-01-19 23:02:52 +0800

    Rewrote some function name

--- docs/index.js
+++ docs/index.js
@@ -1,10 +1,8 @@
 var current_year = (new Date().getFullYear()).toString();
 var targetDate = new Date("Dec 25, " + current_year + " 15:37:25").getTime();
-console.log(targetDate +1)
-var house = document.getElementById("house")
-var christmasSong = ["JingleBellPiano.mp3","OChristmasTree.mp3", "WeWishYouAMerryChristmas.mp3","dingDongMerrily.mp3","herald.mp3"]
-var currSong = 0
-var angle = 0;
+var house = document.getElementById("house");
+var christmasSong = ["JingleBellPiano.mp3","OChristmasTree.mp3", "WeWishYouAMerryChristmas.mp3","dingDongMerrily.mp3","herald.mp3"];
+var currSong = angle = 0;
 
 for (var source of christmasSong) {
   let aud = document.createElement("audio");
@@ -12,16 +10,16 @@ for (var source of christmasSong) {
   document.body.appendChild(aud);
 }
 
-function playsong () {
+function playSong () {
   var song = document.getElementsByTagName("audio")[currSong];
-  song.play()
+  song.play();
   song.onended = function () {
     if (currSong<4) {
       currSong++
     }else {
       currSong=0
     }
-    playsong();
+    playSong();
   }
 }
 
@@ -30,9 +28,10 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var arrow = document.getElementById("creditSlide");
 
+//play the timer when document loaded
 document.body.onload = function () {
-  arrow.addEventListener("click", creditsliding, false);
-  var x = setInterval(function() {
+	arrow.addEventListener("click", creditSlide, false);
+	var x = setInterval(function() {
     var currtime = new Date().getTime();
     var gaps = targetDate - currtime;
 
@@ -43,85 +42,88 @@ document.body.onload = function () {
 
     var timer = document.getElementById("timer")
     timer.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+	//when and in 6 days after christmas snow,playsong and decoration
     if (gaps<= 0 && gaps>=-518400000) {
         merryChristmas()
-        drawdecor()
-        playsong()
+        drawDecor()
+        playSong()
         timer.style.visibility = "hidden"
         canvas.style.visibility = "visible";
     }
+	//7 days before christmas play song and decoration when on peek time before 5 second start peektime animation
     else if (gaps<=6.048e+8) {
-      drawdecor()
-      playsong()
-      canvas.style.visibility = "visible";
-      if (gaps<5 * 60 * 1000 && gaps>0) {
-        timer.style.animation = "peektime 1s infinite";
-      }
+		drawDecor()
+		playSong()
+		canvas.style.visibility = "visible";
+		if (gaps<5 * 60 * 1000 && gaps>0) {
+			timer.style.animation = "peektime 1s infinite";
+		}
     }
     //after christmas
     else if (gaps <=-518400000) {
-      timer.style.visibility = "visible"
-      targetDate = new Date("Dec 25, " + (current_year+1) + " 15:37:25").getTime();
-    }
-  }, 1000)
+		
+		timer.style.visibility = "visible"
+		targetDate = new Date("Dec 25, " + (current_year+1) + " 15:37:25").getTime();
+	}
+}, 1500)
 
 //start snowing at the 7th day before christmas
 
-	//canvas init
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
+//canvas init
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 
-	//canvas dimensions
-	var width = window.innerWidth;
-	var height = window.innerHeight;
-	canvas.width = width;
-	canvas.height = height
+//canvas dimensions
+var canvasW = window.innerWidth;
+var canvasH = window.innerHeight;
+canvas.width = canvasW;
+canvas.height = canvasH
 
-	//snowflake particles
-  density=0
-  if (width>381 || height>500) {
-    density=100
-  }else {
-    density = 50;
-  }
-	var particles = [];
-	for(var i = 0; i < density; i++)
-	{
+//snowflake particles density/the number of it
+density=0
+if (canvasW>381 || canvasH>500) {
+	density=100
+}else {
+	density = 50;
+}
+
+//set value for each particle using an object inside an array 
+var particles = [];
+for (let i = 0; i < density; i++) {
 		particles.push({
-			x: Math.random()*width, //x-coordinate
-			y: Math.random()*height, //y-coordinate
-			r: Math.random()*4+1, //radius
+			x: Math.random()*canvasW,
+			y: Math.random()*canvasH,
+			r: Math.random()*4+1, 
 			d: Math.random()*density
 		})
 	}
+	
+	//drawing the snow
+	function drawSnow() {
+		ctx.clearRect(0, 0, canvasW, canvasH);
 
-	function drawSnow()
-	{
-		ctx.clearRect(0, 0, width, height);
-
+		//move the flakes using 
 		ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
 		ctx.beginPath();
-		for(var i = 0; i < density; i++)
-		{
+		for(let i = 0; i < density; i++) {
 			var p = particles[i];
 			ctx.moveTo(p.x, p.y);
 			ctx.arc(p.x, p.y, p.r, 0, Math.PI*2, true);
 		}
 		ctx.fill();
-		updatedraw();
+		updateDraw();
 	}
-
+	
+	//well since i only need it to flow straight down i only need the new y position using cos...+1 to avoid moving upwards lol
 	var angle = 0;
-	function updatedraw()
-	{
+	function updateDraw() {
 		angle += 0.001;
-    //redraw the snow to the top of the
-		for(var i = 0; i < density; i++)	{
+		for(let i = 0; i < density; i++) {
 			var p = particles[i];
 			p.y += Math.cos(angle+p.d) + 1 + p.r/2;
-			if(p.y > height){
-				if(i%3 > 0) {
-					particles[i] = {x: Math.random()*width, y: -10, r: p.r, d: p.d};
+			if(p.y > canvasH){
+				if(i%4 > 0) {
+					particles[i] = {x: Math.random()*canvasW, y: -10, r: p.r, d: p.d};
 				}
 			}
 		}
@@ -132,7 +134,7 @@ document.body.onload = function () {
 }
 
 //drawing house ,snow floor and tree
-function drawdecor () {
+function drawDecor () {
   var house = document.getElementById("house");
   var snowfloor = document.getElementById("snowFloor")
   var tree = document.getElementById("tree")
@@ -144,30 +146,30 @@ function drawdecor () {
 
 //make the merry christmas text visible
 function merryChristmas() {
-  christmastext = document.getElementById("text")
+  christmastext = document.getElementById("header")
   christmastext.style.visibility="visible"
   setInterval(changeColor,1500)
+}
+
+//function to change the stroke colour of the text
+function changeColor () {
+  var text = document.getElementById("header")
+  text.style.webkitTextStrokeColor = randColorRGB();
 }
 
 //random colour generator in rgb
 function randColorRGB () {
 		var x =[]
-		for (var i=0;i<3;i++) {
+		for (let i=0;i<3;i++) {
 			x[i] = Math.floor(Math.random() * (255 - 155 + 1)) + 155;
 			x[i] = x[i].toString()
 		}
 		return "rgb(" + x[0] + "," + x[1] + "," + x[2] + ")";
 }
 
-//function to change the stroke colour of the text
-function changeColor () {
-  var text = document.getElementById("text")
-  text.style.webkitTextStrokeColor = randColorRGB();
-}
-
 //function to popup the credit section
 
-function creditsliding () {
+function creditSlide () {
   var creditText = document.getElementById("creditText");
   if (creditText.style.opacity==0) {
     //creditText.style.display="block"
